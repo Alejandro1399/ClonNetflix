7f84f885b3c56d60f080f1a42573ae75dde034f7
Alejandro99r <alejoft13@gmail.com>
2021-03-21 21:33:12 -0500

    a

--- lib/Api/Datos.dart
+++ lib/Api/Datos.dart
@@ -1,24 +1,7 @@
 import 'package:omdb_dart/omdb_dart.dart';
 
-void main() {
-  Omdb client = new Omdb("ab958a0a", "Vengadores");
-  client.getMovie();
-  print("Title : ${client.movie.title}"); // print Title of the movie
-  print("Released : ${client.movie.released}"); //print Released Date
-  print("Runtime : ${client.movie.runtime}"); //runtime
-  print("Poster URL : ${client.movie.poster}"); //URL of poster
-  print("Director :${client.movie.director} "); //Director
-  print("Writer :${client.movie.writer} "); //
-  print("Actors :${client.movie.actors} ");
-  print("Plot :${client.movie.plot} ");
-  print("Language :${client.movie.language} ");
-  print("Country :${client.movie.country} ");
-  print("Awards :${client.movie.awards} ");
-  print("Meta Score :${client.movie.metascore} ");
-  print("IMDB Rating :${client.movie.imdbRating} ");
-  print("IMDB Votes :${client.movie.imdbVotes} ");
-  print("IMDB ID :${client.movie.imdbID} ");
-  print("Website :${client.movie.website} ");
-  print("Production :${client.movie.production} ");
-  print("Box Office :${client.movie.boxOffice} ");
+Future<String> pelis(String nombre) async {
+  Omdb poster = new Omdb('ab958a0a', nombre);
+  await poster.getMovie();
+  return '${poster.movie.poster}';
 }
